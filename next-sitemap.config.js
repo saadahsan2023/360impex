af703b7b8c48944cf5e5bcf1c4d681a1e63c78c7
saad ahsan <saadahsankhan890@gmail.com>
2025-06-11 13:38:49 +0500

    SEO on page done

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: 'https://360impex.com', // ← apni real website URL yahan likhein
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+};
+
+module.exports = config;
--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
-// next.config.js
-module.exports = {
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
 };
+
+export default nextConfig;
